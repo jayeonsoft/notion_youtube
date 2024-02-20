dd8a8bd3045b8434dfe5aeb174da9eb2320a9935
KyuchonSong <kyuchon@molcube.xyz>
2024-02-20 12:30:48 -0500

    Auto commit at 2024-02-20 12:30:48

--- git_push.sh
+++ git_push.sh
@@ -1,9 +1,9 @@
 #!/bin/zsh
 
 cd /Users/kyuchonsong/9.test/notion/youtube-list/screenshot/images
-git add .
+git add . > /dev/null
 
 commit_message="Auto commit at $(date +'%Y-%m-%d %H:%M:%S')"
-git commit -m "$commit_message"
+git commit -m "$commit_message" > /dev/null
 
-git push
+git push > /dev/null
